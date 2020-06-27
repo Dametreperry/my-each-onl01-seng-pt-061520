300c95b2d841c9252ec02c0242e9e24dbe919e8e
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-27 05:43:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -12,4 +12,17 @@ collection = [1, 2, 3, 4]
   return collection
 end
   
+  def my_each(array)           
+  i=0
+  while i < array.length
+  yield array [i]
+  i+=1
+end
+    array
+end
+
+collection = [1,2,3,4]
+my_each(collection) do |counter|
+  return counter
+end
   
